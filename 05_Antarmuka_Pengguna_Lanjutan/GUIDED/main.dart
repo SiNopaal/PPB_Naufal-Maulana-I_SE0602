b797398a24ac8b90e67a699e878085545921e601
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-10-16 20:12:53 +0700

    Update main.dart

--- 05_Antarmuka_Pengguna_Lanjutan/GUIDED/main.dart
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:modul_5/custom_scrollview.dart';
+import 'package:modul_5/flexible_expended.dart';
+import 'package:modul_5/jenis_listview.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Custom(),
+    );
+  }
+}
 
