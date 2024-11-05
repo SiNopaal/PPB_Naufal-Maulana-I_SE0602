94168616d2635b38fb746e9278c7f078b4a44aab
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-11-05 17:34:44 +0700

    Update mypage.dart

--- 07_Navigasi & Notifikasi/GUIDED/pages/mypage.dart
+++ 07_Navigasi & Notifikasi/GUIDED/pages/mypage.dart
@@ -1,1 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:modul_07/models/product.dart';
+import 'package:modul_07/pages/detailpage.dart';
+import 'package:intl/intl.dart'; // Tambahkan package intl untuk memformat harga
+
+class MyPage extends StatelessWidget {
+  MyPage({super.key});
+
+  final List<Product> products = [
+    Product(
+        id: 1,
+        nama: 'Mouse Gaming',
+        harga: 300000,
+        gambarUrl:
+            'https://resource.logitechg.com/w_386,ar_1.0,c_limit,f_auto,q_auto,dpr_2.0/d_transparent.gif/content/dam/gaming/en/products/g502x-plus/gallery/g502x-plus-gallery-1-black.png?v=1',
+        deskripsi: 'Mouse gaming yang bagus dan keren'),
+    Product(
+        id: 2,
+        nama: 'Keyboard Mechanical',
+        harga: 400000,
+        gambarUrl:
+            'https://resource.logitech.com/w_1600,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/keyboards/mx-mechanical/gallery/mx-mechanical-keyboard-top-view-graphite-us.png?v=1&quot',
+        deskripsi: 'Keyboard mekanikal memberikan presisi tinggi dengan sakelar individual dan umpan balik taktil'),
+    Product(
+        id: 3,
+        nama: 'HeadSeat Gaming',
+        harga: 500000,
+        gambarUrl:
+            'https://m.media-amazon.com/images/I/61CGHv6kmWL.AC_UF894,1000_QL80.jpg',
+        deskripsi: 'Headset gaming ini memiliki desain yang futuristik dan mencolok'),
+  ];
+
+  final currencyFormatter = NumberFormat.currency(locale: 'id', symbol: 'Rp', decimalDigits: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('MasNopal Store'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[700],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final product = products[index];
+            return Card(
+              elevation: 4,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.all(10),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    product.gambarUrl,
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                title: Text(
+                  product.nama,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        currencyFormatter.format(product.harga),
+                        style: TextStyle(color: Colors.blue[600], fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        product.deskripsi,
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ],
+                  ),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios, color: Colors.blue[600]),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => DetailPage(
+                        product:product ,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
 
