fd768bb4e222f34f01f8c6d6831d7cd694e03ba1
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-12-27 19:32:08 +0700

    Create homepage_screen.dart

---
+++ 14_Data Storage_Bagian 3/GUIDED/homepage_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan5/services/api_services.dart';
+
+class HomepageScreen extends StatefulWidget {
+  const HomepageScreen({super.key});
+
+  @override
+  State<HomepageScreen> createState() => _HomepageScreenState();
+}
+
+class _HomepageScreenState extends State<HomepageScreen> {
+  List<dynamic> _posts = []; // Menyimpan list posts
+  bool _isLoading = false; // Untuk indikator loading
+  final ApiService _apiService = ApiService(); // Instance ApiService
+  // Fungsi untuk menampilkan SnackBar
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  // Fungsi untuk memanggil API dan menangani operasi
+  Future<void> _handleApiOperation(
+      Future<void> operation, String successMessage) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await operation; // Menjalankan operasi API
+      setState(() {
+        _posts = _apiService.posts;
+      });
+      _showSnackBar(successMessage);
+    } catch (e) {
+      _showSnackBar('Error: $e');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('REST API - praktikum 14'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : _posts.isEmpty
+                    ? const Text(
+                        "Tekan tombol GET untuk mengambil data",
+                        style: TextStyle(fontSize: 12),
+                      )
+                    : Expanded(
+                        child: ListView.builder(
+                          itemCount: _posts.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 12.0),
+                              child: Card(
+                                elevation: 4,
+                                child: ListTile(
+                                  title: Text(
+                                    _posts[index]['title'],
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12),
+                                  ),
+                                  subtitle: Text(
+                                    _posts[index]['body'],
+                                    style: const TextStyle(fontSize: 12),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _handleApiOperation(
+                      _apiService.fetchPosts(), 'Data berhasil diambil!'),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+                  child: const Text('GET'),
+                ),
+           
+                ElevatedButton(
+                  onPressed: () => _handleApiOperation(
+                      _apiService.createPost(), 'Data berhasil ditambahkan!'),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  child: const Text('POST'),
+                ),
+                
+                ElevatedButton(
+                  onPressed: () => _handleApiOperation(
+                      _apiService.updatePost(), 'Data berhasil diperbarui!'),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: const Text('UPDATE'),
+                ),
+                
+                ElevatedButton(
+                  onPressed: () => _handleApiOperation(
+                      _apiService.deletePost(), 'Data berhasil dihapus!'),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const Text('DELETE'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
