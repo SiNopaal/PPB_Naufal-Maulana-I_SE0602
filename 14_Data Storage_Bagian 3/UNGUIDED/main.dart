9e77c445badaecdf5f17d67a7107b9da22cc6a6e
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-12-27 19:33:29 +0700

    Create main.dart

---
+++ 14_Data Storage_Bagian 3/UNGUIDED/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan5/screens/homepage_screen_getx.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => HomeScreenGetX(),)
+      ],
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
+      ),
+      // home: HomeScreenGetX(),
+    );
+  }
+}
