4c7c7e5bb86932d6344ac8ca35b69067a378d671
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-12-27 19:24:27 +0700

    Create detail_page.dart

---
+++ 13_Networking/GUIDED/view/detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('halaman detail'),
+      ),
+      body: Center(
+        child: Text('halaman detail'),
+      ),
+    );
+  }
+}
