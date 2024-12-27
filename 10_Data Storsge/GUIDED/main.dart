e3acce6b631303dca88c13d9cf5a3a2da3c68f7e
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-12-27 19:07:10 +0700

    Update main.dart

--- 10_Data Storsge/GUIDED/main.dart
+++ 10_Data Storsge/GUIDED/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:laprak10/view/my_db_view.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyDatabaseView(),
+    );
+  }
+}
