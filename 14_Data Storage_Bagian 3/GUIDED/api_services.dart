c7b69996008a595f7f798b3d230abdc85fa1d0c7
Naufal Maulana Izzuddin <162248360+SiNopaal@users.noreply.github.com>
2024-12-27 19:31:46 +0700

    Create api_services.dart

---
+++ 14_Data Storage_Bagian 3/GUIDED/api_services.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "https://jsonplaceholder.typicode.com";
+  List<dynamic> posts = []; // Menyimpan data post yang diterima
+  // Fungsi untuk GET data
+  Future<void> fetchPosts() async {
+    final response = await http.get(Uri.parse('$baseUrl/posts'));
+    if (response.statusCode == 200) {
+      posts = json.decode(response.body);
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  // Fungsi untuk POST data
+  Future<void> createPost() async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/posts'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'title': 'Flutter Post',
+        'body': 'Ini contoh POST.',
+        'userId': 1,
+      }),
+    );
+    if (response.statusCode == 201) {
+      posts.add({
+        'title': 'Flutter Post',
+        'body': 'Ini contoh POST.',
+        'id': posts.length + 1,
+      });
+    } else {
+      throw Exception('Failed to create post');
+    }
+  }
+
+  // Fungsi untuk UPDATE data
+  Future<void> updatePost() async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/posts/1'),
+      body: json.encode({
+        'title': 'Updated Title',
+        'body': 'Updated Body',
+        'userId': 1,
+      }),
+    );
+    if (response.statusCode == 200) {
+      final updatedPost = posts.firstWhere((post) => post['id'] == 1);
+      updatedPost['title'] = 'Updated Title';
+      updatedPost['body'] = 'Updated Body';
+    } else {
+      throw Exception('Failed to update post');
+    }
+  }
+
+  // Fungsi untuk DELETE data
+  Future<void> deletePost() async {
+    final response = await http.delete(
+      Uri.parse('$baseUrl/posts/1'),
+    );
+    if (response.statusCode == 200) {
+      posts.removeWhere((post) => post['id'] == 1);
+    } else {
+      throw Exception('Failed to delete post');
+    }
+  }
+}
